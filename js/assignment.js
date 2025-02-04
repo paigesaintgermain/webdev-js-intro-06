1d0fb728909f1c072d507a04339c1255e03e3829
Paige Saint Germain <paigejerin@gmail.com>
2025-02-03 20:54:14 -0500

    completed js assignment 06

--- js/assignment.js
+++ js/assignment.js
@@ -1,15 +1,20 @@
-"use strict";
+'use strict';
 
-const submissionBtn = document.getElementById("submission-btn");
-const evenOrOddElement = document.getElementById("even-or-odd");
-const sumTheNumbersElement = document.getElementById("sum-the-numbers");
-const createNumberArrayElement = document.getElementById("create-number-array");
+const submissionBtn = document.getElementById('submission-btn');
+const evenOrOddElement = document.getElementById('even-or-odd');
+const sumTheNumbersElement = document.getElementById('sum-the-numbers');
+const createNumberArrayElement = document.getElementById('create-number-array');
 
 function evenOrOdd() {
     const num = 3;
     // Write the logic to decide if the variable "num" is even or odd
-    // and set the element's value the string "Even" or "Odd" accordingly
+    // and set the element's value to the string "Even" or "Odd" accordingly
 
+    if (3 % 2 !== 0) {
+        evenOrOddElement.innerHTML = 'Odd';
+    } else {
+        evenOrOddElement.innerHTML = 'Even';
+    }
 }
 
 function sumTheNumbers() {
@@ -18,6 +23,12 @@ function sumTheNumbers() {
     // using a for loop. Check the expected output
     // on the assignment page
 
+    let total = 0;
+    for (let index = 1; index <= 10; index++) {
+        total = total + index;
+    }
+
+    sumTheNumbersElement.innerHTML = total;
 }
 
 function createNumberArray() {
@@ -27,13 +38,20 @@ function createNumberArray() {
     // to numberArray each iteration. Check the expected output
     // on the assignment page
 
+    for (let index = 1; index <= 10; index++) {
+        numberArray.push(index);
+    }
+
+    createNumberArrayElement.innerHTML = numberArray;
 }
 
 function render() {
     // Call the created functions
-
+    evenOrOdd();
+    sumTheNumbers();
+    createNumberArray();
 }
 
-submissionBtn.addEventListener("click", function () {
+submissionBtn.addEventListener('click', function () {
     render();
 });
--- js/index.js
+++ js/index.js
@@ -1,3 +1,43 @@
 // Prevent us from attempting to use variables
 // that are not declared
-"use strict"
+'use strict';
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+
+// let counter = 1;
+// const diceGoal = 6;
+
+// while (counter < 20) {
+//     let diceRoll = Math.floor(Math.random() * 6) + 1;
+
+//     console.log(`Roll: ${diceRoll}`);
+//     if (diceRoll === diceGoal) {
+//         console.log(`Got a ${diceRoll} in only ${counter} rolls!`);
+//         break;
+//     }
+
+//     counter++;
+// }
+
+
+// let alsoCounter = 1;
+// let diceRemoved = 0;
+
+// const diceRemove = 1;
+
+// do {
+//     let diceRoll = Math.floor(Math.random() * 6) + 1;
+
+//     alsoCounter++;
+
+//     if (diceRoll === diceRemove) {
+//         diceRemoved++;
+//         continue;
+//     }
+
+//     console.log(`Roll: ${diceRoll}`);
+// } while (alsoCounter <= 20);
+
+// console.log(`Removed ${diceRemoved} dice that rolled a ${diceRemove}.`);
